a2d654cf0ef2790e1c75cf74bc16ff94e81b2454
Vargheseeldo10 <vargheseeldo1010@gmail.com>
2024-08-28 15:12:43 +0530

    Initial commit

---
+++ mern-todo-app/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/todo-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const taskRoutes = require('./routes/tasks');
+app.use('/api/tasks', taskRoutes);
